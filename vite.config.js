4cbacfa7f6cb276fd89021c4492d854d4891da55
g0st-r00t-x <g0str00tx@gmail.com>
2025-03-16 20:14:23 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import laravel, { refreshPaths } from "laravel-vite-plugin";
 
 export default defineConfig({
-    server: {
-        https: true
-    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
